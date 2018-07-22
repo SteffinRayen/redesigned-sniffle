102bab2d9f14ba683bba07e20b74fdc84b255d54
SteffinRayen <stefinrayen@gmail.com>
2018-07-22 19:37:56 +0530

    Changed Admin view for Table

--- website/sample/admin.py
+++ website/sample/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ClusterData, ProjectData, ModuleData, ErrorData
+
+class ClusterAdmin(admin.ModelAdmin):
+    list_display = ('cluster_id','cluster_description')
+    list_per_page = 25
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('project_id', 'project_description')
+    list_per_page = 25
+
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('module_id','module_description')
+    list_per_page = 25
+
+class ErrorAdmin(admin.ModelAdmin):
+    list_display = ('error_id', 'error_description','error_mitigation','screenshot_link','author','error_validated','error_validator','editors_list')
+    list_per_page = 25
+
+
+
+
+admin.site.register(ClusterData, ClusterAdmin)
+admin.site.register(ProjectData, ProjectAdmin)
+admin.site.register(ModuleData, ModuleAdmin)
+admin.site.register(ErrorData, ErrorAdmin)
