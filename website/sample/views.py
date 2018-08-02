56fd2d55a003da6a5f1d464776bd84d1e74e9454
SteffinRayen <stefinrayen@gmail.com>
2018-08-02 19:21:56 +0530

    Rendered table with content

--- website/sample/views.py
+++ website/sample/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import ClusterData, ModuleData, ProjectData, ErrorData
 # Create your views here.
 
-def login(request):
-    context = {}
-    return render(request, 'sample/login.html', context)
-
 def search(request):
-    context = {}
+    all_error_data = ErrorData.objects.all()
+    context = {
+        'all_error_data' : all_error_data,
+    }
     return render(request, 'sample/search.html', context)
 
 def upload(request):
@@ -16,7 +15,7 @@ def upload(request):
     return render(request, 'sample/upload.html', context)
 
 def index(request):
-    return HttpResponse("<h1>Hello, world. You're at the sample home.</h1>")
+    return HttpResponse("<h1>Hello, world. You're at the sample home page.</h1>")
 
 def handler404(request, exception, template_name='404.html'):
     return render(request, '404.html', status=404)
