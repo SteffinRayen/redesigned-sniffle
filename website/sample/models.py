b6cd43f5bfd08cc58007feced81fae1a277723f0
SteffinRayen <stefinrayen@gmail.com>
2018-08-03 21:47:54 +0530

    Rest of model

--- website/sample/models.py
+++ website/sample/models.py
@@ -5,9 +5,10 @@ from django.utils import timezone
 
 class ClusterData(models.Model):
     cluster_id = models.IntegerField(unique=True, default=timezone.now)
-    cluster_description = models.CharField(max_length = 200)
+    cluster_name = models.CharField(max_length = 1000)
+    cluster_description = models.CharField(max_length = 1000)
     def __str__(self):
-        return "%s" % (self.cluster_id)
+        return "%s" % (self.cluster_name)
     class Meta:
         ordering = ('cluster_id',)
         verbose_name = "Cluster Data"
@@ -16,9 +17,10 @@ class ClusterData(models.Model):
 class ProjectData(models.Model):
     cluster_id = models.ForeignKey(ClusterData, to_field='cluster_id', on_delete=models.CASCADE)
     project_id = models.CharField(max_length=10,unique=True,default=timezone.now)
-    project_description = models.CharField(max_length = 200)
+    project_name = models.CharField(max_length = 1000)
+    project_description = models.CharField(max_length = 1000)
     def __str__(self):
-        return "%s" % (self.project_id)
+        return "%s" % (self.project_name)
     class Meta:
         ordering = ('project_id',)
         verbose_name = "Project Data"
@@ -27,9 +29,10 @@ class ProjectData(models.Model):
 class ModuleData(models.Model):
     project_id = models.ForeignKey(ProjectData, to_field='project_id', on_delete=models.CASCADE)
     module_id = models.CharField(max_length=10,unique=True,default=timezone.now)
-    module_description = models.CharField(max_length = 200)
+    module_name = models.CharField(max_length = 1000)
+    module_description = models.CharField(max_length = 1000)
     def __str__(self):
-        return "%s" % (self.module_id)
+        return "%s" % (self.module_name)
     class Meta:
         ordering = ('module_id',)
         verbose_name = "Module Data"
@@ -38,15 +41,16 @@ class ModuleData(models.Model):
 class ErrorData(models.Model):
     module_id = models.ForeignKey(ModuleData, to_field='module_id', on_delete=models.CASCADE)
     error_id = models.CharField(max_length=10,unique=True,default=timezone.now)
-    error_description = models.CharField(max_length = 200)
+    error_name = models.CharField(max_length = 1000)
+    error_description = models.CharField(max_length = 1000)
     error_mitigation = models.CharField(max_length = 1000)
-    screenshot_link = models.CharField(max_length = 200)
-    author = models.CharField(max_length = 200)
+    screenshot_link = models.CharField(max_length = 1000)
+    author = models.CharField(max_length = 1000)
     error_validated = models.BooleanField(default=False)
-    error_validator = models.CharField(max_length = 200)
+    error_validator = models.CharField(max_length = 1000)
     editors_list = models.CharField(max_length = 1000)
     def __str__(self):
-        return "%s" % (self.error_id)
+        return "%s" % (self.error_name)
     class Meta:
         ordering = ('error_id',)
         verbose_name = "Error Data"
@@ -54,8 +58,8 @@ class ErrorData(models.Model):
 
 
 class UserData(models.Model):
-    user_id = models.CharField(max_length = 200)
-    password = models.CharField(max_length = 200)
+    user_id = models.CharField(max_length = 1000)
+    password = models.CharField(max_length = 1000)
     def __str__(self):
         return "%s" % (self.user_id)
     class Meta:
