5b74c32e4571c41adb2cfc3f73a10a45e3920f1e
SteffinRayen <stefinrayen@gmail.com>
2018-08-03 08:20:27 +0530

    Sent all data for listing #need_to_sort_later

--- website/sample/views.py
+++ website/sample/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ClusterData, ModuleData, ProjectData, ErrorData
+from django.urls import reverse_lazy #https://simpleisbetterthancomplex.com/tutorial/2018/01/29/how-to-implement-dependent-or-chained-dropdown-list-with-django.html
 # Create your views here.
 
 def search(request):
+    all_cluster_data = ClusterData.objects.all()
+    all_module_data = ModuleData.objects.all()
+    all_project_data = ProjectData.objects.all()
     all_error_data = ErrorData.objects.all()
     context = {
+        'all_cluster_data' : all_cluster_data,
+        'all_module_data' : all_module_data,
+        'all_project_data' : all_project_data,
         'all_error_data' : all_error_data,
     }
     return render(request, 'sample/search.html', context)
